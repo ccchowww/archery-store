ea9ac50498620dbbbb4d5facdfb51687b5c08bf9
ccchowww <ccchowww@gmail.com>
2018-08-27 15:48:45 +0800

    express and mongoose boilerplate

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+const testes = require('./routes/api/testes');
+
+//Bodyparser middleware
+app.use(bodyParser.json());
+
+//mongodb uri
+const db = require('./config/keys').mongoURI;
+
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+app.use('/', testes);
+
+//serve static if production
+if(process.env.NODE_ENV == 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
+
+const port = process.env.PORT || 5000;
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
